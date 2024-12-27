ae744aba74bc749d81187735043fba3ae432c160
Abdu Zoghbi <a.zoghbi@nasa.gov>
2024-12-27 08:43:03 -0500

    add more testing ci [skip ci]

--- tests/test_astro-default.py
+++ tests/test_astro-default.py
@@ -2,6 +2,7 @@ import unittest
 import subprocess
 import sys
 import os
+from packaging import version
 
 sys.path.insert(0, os.getcwd())
 from test_base_image import CommonTests
@@ -11,6 +12,12 @@ class Test_astro_default(unittest.TestCase, CommonTests):
 
     def test_conda_env_file(self):
         self._test_conda_env_file('astro-default')
+    
+    def test_check_packages(self):
+        import tractor
+        import astrometry
+        import lsdb
+        self.assertLess(version.parse(lsdb.__version__), version.parse('0.4'))
     
 
 
