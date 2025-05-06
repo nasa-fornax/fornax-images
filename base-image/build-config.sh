3611c21ed558707b894314e3cd4836994dac50b1
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-05-05 22:11:19 -0400

    switch to code-server

--- base-image/build-config.sh
+++ base-image/build-config.sh
@@ -10,8 +10,3 @@ mv ~/build/overrides.json $CONDA_DIR/envs/$env/share/jupyter/lab/settings/
 # disable the announcement extension
 mamba run -n $env jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
 
-# install openvscode-server
-cd /tmp/
-wget -q https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v1.98.2/openvscode-server-v1.98.2-linux-x64.tar.gz
-tar -zxf openvscode-server-v1.98.2-linux-x64.tar.gz
-mv openvscode-server-v1.98.2-linux-x64 /opt/openvscode-server
