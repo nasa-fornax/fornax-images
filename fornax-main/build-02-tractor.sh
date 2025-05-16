01010cb207784b318537c49e9f025bef53ed84a3
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-05-16 12:51:39 -0400

    uncomment tractor build

--- fornax-main/build-02-tractor.sh
+++ fornax-main/build-02-tractor.sh
@@ -1,34 +1,34 @@
-# #!/bin/bash
-# # Build astronomy.net and tractor
+#!/bin/bash
+# Build astronomy.net and tractor
 
-# set -e 
-# set -o pipefail
+set -e 
+set -o pipefail
 
-# pythonenv=py-multiband_photometry
-# astrometry_commit=1b7d716
-# tractor_commit=8059ae0
+pythonenv=py-multiband_photometry
+astrometry_commit=1b7d716
+tractor_commit=8059ae0
 
-# mamba install cython
-# mamba clean -yaf
+mamba install cython
+mamba clean -yaf
 
-# # Install astrometry.net and tractor
-# cd /tmp
-# git clone https://github.com/dstndstn/astrometry.net.git
-# cd astrometry.net
-# git config --global --add safe.directory $PWD
-# git checkout $astrometry_commit
-# make
-# make py
-# make extra
-# make install INSTALL_DIR=${ENV_DIR}/${pythonenv}
-# mv ${ENV_DIR}/${pythonenv}/lib/python/astrometry \
-#    ${ENV_DIR}/${pythonenv}/lib/python3.??/
+# Install astrometry.net and tractor
+cd /tmp
+git clone https://github.com/dstndstn/astrometry.net.git
+cd astrometry.net
+git config --global --add safe.directory $PWD
+git checkout $astrometry_commit
+make
+make py
+make extra
+make install INSTALL_DIR=${ENV_DIR}/${pythonenv}
+mv ${ENV_DIR}/${pythonenv}/lib/python/astrometry \
+   ${ENV_DIR}/${pythonenv}/lib/python3.??/
 
-# cd /tmp
-# git clone https://github.com/dstndstn/tractor.git
-# cd tractor
-# git checkout $tractor_commit
-# python setup.py build_ext --inplace --with-cython
-# pip install --no-cache-dir . --target ${ENV_DIR}/${pythonenv}/lib/python3.??/
-# cd $HOME
-# rm -rf /tmp/astrometry.net /tmp/tractor
+cd /tmp
+git clone https://github.com/dstndstn/tractor.git
+cd tractor
+git checkout $tractor_commit
+python setup.py build_ext --inplace --with-cython
+pip install --no-cache-dir . --target ${ENV_DIR}/${pythonenv}/lib/python3.??/
+cd $HOME
+rm -rf /tmp/astrometry.net /tmp/tractor
