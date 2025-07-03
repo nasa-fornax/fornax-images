1541acaaaf66ecc8be5383e60485c7b2678d6f14
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-07-03 15:16:43 -0400

    update tests for jupyter-base

--- tests/test_jupyter_base.py
+++ tests/test_jupyter_base.py
@@ -2,19 +2,20 @@ import unittest
 import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
-from common import CommonTests, conda_dir  # noqa E402
+from common import CommonTests  # noqa E402
+from common import jupyter_env, jupyter_root  # noqa E402
 
 
 class Test_jupyter_base(unittest.TestCase):
 
     def test_python_path(self):
-        CommonTests._test_python_path('', is_conda=True)
+        CommonTests._test_python_path(jupyter_env, jupyter_root)
 
     def test_which_python(self):
-        CommonTests._test_which_python('', is_conda=True)
+        CommonTests._test_which_python(jupyter_env, jupyter_root)
 
-    def test_base_env(self):
-        CommonTests._test_conda_env_file('base', f'{conda_dir}/base-lock.yml')
+    def test_jupyter_env(self):
+        CommonTests._test_uv_env_file(jupyter_env, jupyter_root)
 
     def test_stack_files(self):
         """Check for file from jupyter stack"""
@@ -24,6 +25,19 @@ class Test_jupyter_base(unittest.TestCase):
             '/etc/jupyter/docker_healthcheck.py',
         ]:
             assert os.path.exists(file)
+
+    def test_stack_files_jupyter(self):
+        """Check that the start files use the correct jupyter"""
+        for file in [
+            '/usr/local/bin/start-notebook.py',
+            '/usr/local/bin/start-singleuser.py',
+        ]:
+            with open(file) as fp:
+                lines = fp.readlines()
+                assert any(
+                    [f'"{jupyter_root}/{jupyter_env}/bin/jupyter'
+                        in line for line in lines]
+                )
 
 
 if __name__ == "__main__":
