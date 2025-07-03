eb73231e7680c31b86edb6392ed0713c24d58e51
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-07-03 17:28:45 -0400

    update fornax-base tests

--- tests/test_fornax_base.py
+++ tests/test_fornax_base.py
@@ -1,28 +1,30 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
-from common import CommonTests, conda_dir, env_dir  # noqa E402
+from common import CommonTests  # noqa E402
+from common import uv_root, jupyter_env, jupyter_root  # noqa E402
+
 
 default_kernel = 'python3'
 
 
 def test_python_path():
-    CommonTests._test_python_path(default_kernel, is_conda=False)
+    CommonTests._test_python_path(default_kernel, uv_root)
 
 
 def test_which_python():
-    CommonTests._test_which_python(default_kernel, is_conda=False)
+    CommonTests._test_which_python(default_kernel, uv_root)
 
 
 def test_env_file():
-    CommonTests._test_uv_env_file(default_kernel)
+    CommonTests._test_uv_env_file(default_kernel, uv_root)
 
 
 def test_env_vars():
     assert os.environ['DEFAULT_ENV'] == default_kernel
     assert os.environ['ENV_DIR'] == '/opt/envs'
-    assert os.environ['ENV_DIR'] == env_dir
+    assert os.environ['ENV_DIR'] == uv_root
 
 
 def test_base_env():
-    CommonTests._test_conda_env_file('base', f'{conda_dir}/base-lock.yml')
+    CommonTests._test_uv_env_file(jupyter_env, jupyter_root)
