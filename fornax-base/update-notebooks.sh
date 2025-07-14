f74734060db256ef3d9a47c2171ea55d5c396b5d
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-07-14 08:40:46 -0400

    separate link-notebooks from update-notebooks

--- fornax-base/update-notebooks.sh
+++ fornax-base/update-notebooks.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# if something fails, keep going, we don't want to the stop the server from loading
-set -e
 timeout=10
 
 # if NOTEBOOK_DIR is not defined; return or exit
@@ -36,16 +34,7 @@ if test -f $JUPYTER_DIR/introduction.html && ! test -L $NOTEBOOK_DIR/introductio
     cp $JUPYTER_DIR/introduction.html $NOTEBOOK_DIR
 fi
 
-# make symlink for NOTEBOOD_DIR in HOME
-cd $HOME
-home_nb_dir=fornax-notebooks
-# if there is a folder and is not a symlink, rename it
-if test -d $home_nb_dir && ! test -L $home_nb_dir; then
-    mv $home_nb_dir ${home_nb_dir}.rename
-fi
-if ! test -d $home_nb_dir; then
-    ln -s $NOTEBOOK_DIR $home_nb_dir
-fi
+
 
 # reset location
 cd $HOME
