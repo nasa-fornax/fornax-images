40755020930549c28d80bc7b7a30ff87173a8c12
Abdu Zoghbi <a.zoghbi@nasa.gov>
2025-07-15 11:08:40 -0400

    use env_root instead of uv_root in test files

--- tests/test_fornax_main.py
+++ tests/test_fornax_main.py
@@ -6,7 +6,7 @@ import pytest
 
 sys.path.insert(0, os.path.dirname(__file__))
 from common import CommonTests, change_dir  # noqa E402
-from common import uv_root, jupyter_env, jupyter_root  # noqa E402
+from common import env_root, jupyter_env, jupyter_root  # noqa E402
 
 default_kernel = 'python3'
 
@@ -38,21 +38,21 @@ notebooks = {
 
 
 def test_python_path():
-    CommonTests._test_python_path(default_kernel, uv_root)
+    CommonTests._test_python_path(default_kernel, env_root)
 
 
 def test_which_python():
-    CommonTests._test_which_python(default_kernel, uv_root)
+    CommonTests._test_which_python(default_kernel, env_root)
 
 
 def test_env_file():
-    CommonTests._test_uv_env_file(default_kernel, uv_root)
+    CommonTests._test_uv_env_file(default_kernel, env_root)
 
 
 def test_env_vars():
     assert os.environ['DEFAULT_ENV'] == default_kernel
     assert os.environ['ENV_DIR'] == '/opt/envs'
-    assert os.environ['ENV_DIR'] == uv_root
+    assert os.environ['ENV_DIR'] == env_root
 
 
 def test_base_env():
@@ -67,7 +67,7 @@ def test_notebooks_folder():
 
 @pytest.mark.parametrize("notebook",  list(notebooks.keys()))
 def test_check_packages(notebook):
-    CommonTests._test_uv_env_file(notebooks[notebook]['env'], uv_root)
+    CommonTests._test_uv_env_file(notebooks[notebook]['env'], env_root)
 
 
 @pytest.mark.parametrize("notebook", list(notebooks.keys()))
@@ -102,5 +102,5 @@ def test_imports(notebook):
                 re.sub(r'\n|\\', '', line) for line in imports]))
 
         CommonTests.run_cmd(
-            f'{uv_root}/{env}/bin/python imports_{py_filename}'
+            f'{env_root}/{env}/bin/python imports_{py_filename}'
         )
