63777f4abc99c67061825ca92a87c1e429610435
David Turner <turne540@msu.edu>
2025-10-10 09:28:01 -0400

    Altered the build_sas script to move the SAS lib/data directory to the support data directory, rather than delete it

--- fornax-hea/build-sas.sh
+++ fornax-hea/build-sas.sh
@@ -201,16 +201,18 @@ EOF
 ###########################################################
 
 
-################### Remove XMM SAS data ###################
-# TODO UPDATE THIS APPROACH WHEN AMI IS IN PRODUCTION
-# Here we delete an existing directory and put a symlink in its place, in order to minimize the footprint of the
-#  Fornax-Hea image. This data directory IS necessary for SAS to work, but we have made sure it is in place on
-#  the Fornax system; this approach does break the Fornax-Hea SAS install anywhere but on Fornax, so we will improve
-#  this approach as soon as possible
-rm -r $ENV_DIR/$ENV_NAME/${sas_install_dir}/lib/data
+################# Move XMM SAS data ##################
+# Here we move an existing directory and put symlink it back to its original location - this is to minimize the
+#  size of the AMI environment images (as they will just use the support-data directory that is already on
+#  Fornax), and make sure that the Fornax-Hea image will still have access to the support data when it runs on
+#  another platform.
+# This data directory IS necessary for SAS to work
+mv $ENV_DIR/$ENV_NAME/${sas_install_dir}/lib/data $SUPPORT_DATA_DIR/xmmsas-${sas_version}/sas_data
+# And then symlink it back
 ln -s $SUPPORT_DATA_DIR/xmmsas-${sas_version}/sas_data $ENV_DIR/$ENV_NAME/${sas_install_dir}/lib/data
 
-# We also remove the documentation source and build, once again to save space
+# We also remove the documentation source and build, once again to save space (don't bother symlinking this one,
+#  the documentation are very easily found online).
 rm -r $ENV_DIR/$ENV_NAME/${sas_install_dir}/doc
 ######################################################
 
