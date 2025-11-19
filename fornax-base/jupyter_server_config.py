34638dcf0be180e898788cbbb6bc98504d5687fb
Abdu Zoghbi <astrozoghbi@gmail.com>
2025-11-19 13:29:50 -0500

    change terminal cull settings

--- fornax-base/jupyter_server_config.py
+++ fornax-base/jupyter_server_config.py
@@ -2,5 +2,5 @@
 # c = get_config() assumed called.
 
 # cull terminals
-c.TerminalManager.cull_inactive_timeout = 3600
-c.TerminalManager.cull_interval = 600
+c.TerminalManager.cull_inactive_timeout = 900
+c.TerminalManager.cull_interval = 300
