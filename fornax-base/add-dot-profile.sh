1bbc5c6bf8746c533c15173d746103d4a04852e7
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-01-07 16:02:44 -0500

    make fornax-main and its scripts username-agnostic and fix permissions

--- fornax-base/add-dot-profile.sh
+++ fornax-base/add-dot-profile.sh
@@ -4,10 +4,10 @@
 # JL terminals source ~/.profile not ~/.bashrc
 # But some user software may need ~/.bashrc (e.g. rust, julia)
 
-if [ ! -f ~/.profile ]; then
-    cat <<PROFILE > ~/.profile
-if [ -f ~/.bashrc ]; then
-    source ~/.bashrc
+if [ ! -f /home/$NB_USER/.profile ]; then
+    cat <<PROFILE > /home/$NB_USER/.profile
+if [ -f /home/$NB_USER/.bashrc ]; then
+    source /home/$NB_USER/.bashrc
 fi
 PROFILE
 fi
