b2e9732b3e442fad2e9f454055dc55d6b3eec131
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-01-13 17:35:26 -0500

    add r and julia

---
+++ fornax-main/build-r.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Add R kernel
+set -eux
+set -o pipefail
+
+cd /tmp/
+
+envfile=conda-Renv.yml
+envname=Renv
+
+cat <<EOF > $envfile
+name: heasoft
+channels:
+  - conda-forge
+  - nodefaults
+dependencies:
+  - r
+  - r-irkernel
+EOF
+
+micromamba create -y -p $ENV_DIR/Renv -f $envfile
+
+# add the environment as a jupyter kernel
+micromamba run -p $ENV_DIR/Renv R -e "IRkernel::installspec(prefix = Sys.getenv('JUPYTER_DIR'))"
+micromamba clean -yaf
