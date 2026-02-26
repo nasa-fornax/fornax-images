917a3f335577f3001622f8969461e299ffe6f119
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-02-26 09:29:05 -0500

    Add tectonic as tex processor to fix #18

--- fornax-base/jupyter_server_config.py
+++ fornax-base/jupyter_server_config.py
@@ -4,3 +4,7 @@
 # cull terminals
 c.TerminalManager.cull_inactive_timeout = 3600
 c.TerminalManager.cull_interval = 300
+
+# for nbconvert
+c.PDFExporter.latex_command = ['tectonic', '{filename}']
+c.PDFExporter.bib_command = ['/bin/true', '{filename}']
