0208348b25e35300cafaeb02eccf0b2a322abb1b
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-06-09 15:59:57 -0400

    a few fixes to fornax-base

--- fornax-base/scripts/_activate.sh
+++ fornax-base/scripts/_activate.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/bash
 # A script to activate the default environment from DEFAULT_ENV
 
-if [ -f $ENV_DIR/$DEFAULT_ENV/bin/activate ]; then
-  echo activating python in \$ENV_DIR/\$DEFAULT_ENV
-  source $ENV_DIR/$DEFAULT_ENV/bin/activate
-elif [ -d $ENV_DIR/$DEFAULT_ENV/conda-meta ]; then
-  echo activating python in $ENV_DIR/$DEFAULT_ENV
+
+if [ "$DEFAULT_ENV" == "jupyter" ]; then
+  env_dir=$JUPYTER_DIR
+else
+  env_dir=$ENV_DIR/$DEFAULT_ENV
+fi
+
+if [ -f $env_dir/bin/activate ]; then
+  echo activating python in $env_dir
+  source $env_dir/bin/activate
+elif [ -d $env_dir/conda-meta ]; then
+  echo activating python in $env_dir
   micromamba activate $DEFAULT_ENV
 else
-  echo DEFAULT_ENV=$DEFAULT_ENV not found
+  echo DEFAULT_ENV=$DEFAULT_ENV not found in $env_dir
 fi
