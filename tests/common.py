eb976547b5b8b3018f9a1f990cea44f6d266f06c
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-07-10 13:01:25 -0400

    cli test runs now raise CalledProcessError instead of RuntimeError

--- tests/common.py
+++ tests/common.py
@@ -15,14 +15,10 @@ class CommonTests:
     @staticmethod
     def run_cmd(command, env=None, **runargs):
         """Run shell command"""
-        result = subprocess.run(command, shell=True, check=False, text=True,
+        # Failures raise subprocess.CalledProcessError
+        # callers can intercept it.
+        result = subprocess.run(command, shell=True, check=True, text=True,
                                 capture_output=True, env=env, **runargs)
-        if result.returncode != 0:
-            sep = '\n' + ('+'*20) + '\n'
-            raise RuntimeError(
-                (f'*** ERROR running: {command}' + sep + result.stdout
-                 + sep + result.stderr)
-                )
         return result
 
     @staticmethod
