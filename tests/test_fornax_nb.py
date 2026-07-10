358c0ba58c78375888dd941e618a438f310813c0
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-07-10 13:01:48 -0400

    add a pytest mark for full notebook runs

--- tests/test_fornax_nb.py
+++ tests/test_fornax_nb.py
@@ -121,6 +121,7 @@ def test_notebook_permissions(notebook):
     assert os.access(f'{notebook_dir}/{nb_path}', os.W_OK)
 
 
+@pytest.mark.full_notebook
 @pytest.mark.parametrize("notebook",  list(notebooks.keys()))
 def test_run_notebooks(notebook):
     """Do a full run of the notebook"""
