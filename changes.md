ff13f7f617f0416ceb4f10f87bc1ab230c58389c
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-07-30 12:53:50 -0400

    disable JL rebuild checks

--- changes.md
+++ changes.md
@@ -5,6 +5,7 @@
   - Security updates.
   - Fix issue where some ciao tasks fail with numpy>=2.5 (#192).
   - Move several jupyter extension to deferred mode.
+  - Use scratch for jupyter local files instead of ~.
 
 ## 26.0717
 - Upgrade fermitools to 2.5.1 and fix the fermipy dependencies.
--- fornax-jupyter/jupyter_server_config.py
+++ fornax-jupyter/jupyter_server_config.py
@@ -8,3 +8,11 @@ c.TerminalManager.cull_interval = 300
 # for nbconvert
 c.PDFExporter.latex_command = ['tectonic', '{filename}']
 c.PDFExporter.bib_command = ['/bin/true', '{filename}']
+
+# Disable Build Check via Jupyter
+c.ServerApp.tornado_settings = {
+    "page_config_data": {
+        "buildCheck": False,
+        "buildAvailable": False
+    }
+}
