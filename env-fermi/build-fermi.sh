dbdd5386e50b3c52e0aa205fab47b39d15257a91
Abdu Zoghbi <astrozoghbi@gmail.com>
2026-08-27 17:28:02 -0400

    add back the region constrain to fix fermipy, gammapy is still 2.1

--- env-fermi/build-fermi.sh
+++ env-fermi/build-fermi.sh
@@ -24,8 +24,6 @@ rm -rf * > /dev/null 2>&1
 
 # TODO IF UPDATING, CHECK WHETHER 'gammapy' HAS A RELEASE VERSION HIGHER THAN 2.1, IF
 #  YES THEN YOU CAN PROBABLY REMOVE THE REGION<0.12 RESTRICTION.
-# TODO Python=3.11 is driven by fermitools, once it has non-dev conda releases
-#  built for versions later than 3.11 this should be replaced by 'python=$PYTHON_VERSION'
 cat <<EOF > conda-fermi.yml
 name: fermi
 channels:
@@ -35,6 +33,7 @@ dependencies:
   - python=3.12
   - regions<0.12
   - fermitools=2.5.2
+  - region<0.12
   - fermipy
   - pip
   - pip:
